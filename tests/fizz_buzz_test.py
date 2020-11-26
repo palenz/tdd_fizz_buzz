143b27e8dce62953a65ced2a5a065bab96308aa8
Juan <jpalenzuela@outlook.com>
2020-11-26 11:44:56 +0000

    second test passed

--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -5,4 +5,7 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_fizz_buzz_3_returns_fizz(self):
         self.assertEqual('fizz', fizz_buzz(3))
+
+    def test_fizz_buzz_5_returns_buzz(self):
+        self.assertEqual('buzz', fizz_buzz(5))
  
