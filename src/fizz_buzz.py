874dae2c97df5ca110fbb878253e016a9ce53467
Juan <jpalenzuela@outlook.com>
2020-11-26 12:05:15 +0000

    fizzbuzz complete :)

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,7 +1,11 @@
 def fizz_buzz(number):
-    if number == 3:
+    if number % 15 == 0:
+        return 'fizzbuzz'
+    if number % 3 == 0:
         return 'fizz'
+    elif number % 5 == 0:
+        return 'buzz'
     elif number == 5:
         return 'buzz'
     else:
-        return 'fizzbuzz'
+        return str(number)
--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -6,9 +6,21 @@ class TestFizzBuzz(unittest.TestCase):
     def test_fizz_buzz_3_returns_fizz(self):
         self.assertEqual('fizz', fizz_buzz(3))
 
+    def test_fizz_buzz_6_returns_fizz(self):
+        self.assertEqual('fizz', fizz_buzz(6))
+
     def test_fizz_buzz_5_returns_buzz(self):
         self.assertEqual('buzz', fizz_buzz(5))
 
+    def test_fizz_buzz_10_returns_buzz(self):
+        self.assertEqual('buzz', fizz_buzz(10))
+
     def test_fizz_buzz_15_returns_fizzbuzz(self):
         self.assertEqual('fizzbuzz', fizz_buzz(15))
+
+    def test_fizz_buzz_45_returns_fizzbuzz(self):
+        self.assertEqual('fizzbuzz', fizz_buzz(45))
+
+    def test_fizz_buzz_4_returns_4str(self):
+        self.assertEqual('4', fizz_buzz(4))
  
