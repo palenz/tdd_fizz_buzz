8b74e699d442f4b0ac0210752026018fb87cde6a
Juan <jpalenzuela@outlook.com>
2020-11-26 11:22:25 +0000

    initial setup complete

---
+++ run_tests.py
@@ -0,0 +1,5 @@
+import unittest
+from test.fizz_buzz_test import TestFizzBuzz
+
+if __name__=='__main__':
+    unittest.main()
---
+++ tests/fizz_buzz_test.py
@@ -0,0 +1,6 @@
+import unittest
+from src.fizz_buzz import fizz_buzz
+
+class TestFizzBuzz(unittest.TestCase):
+
+    
